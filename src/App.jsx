a5c96d7322971b7f716defe762555e8f4227811c
ABHISHEK SINGH <abhisheksingh160405@gmail.com>
2025-06-11 21:05:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './app.css'
+import React from 'react';
+
+
+function WYD()
+{
+  return(
+    <div class='container'>
+      <div class='wud'>WUD ?</div>
+    </div>
+  )
+}
+
+export default WYD;
